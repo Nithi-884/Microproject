3b78cf5d67d6918b7f222d752f7b2a05ab6f95b9
Nithin Narayanan <75129591+ryn018@users.noreply.github.com>
2025-04-18 17:11:53 +0530

    del

--- django-app/employeesalaryapp/salaryapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SalaryappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "salaryapp"
--- django-app/employeesalaryapp/salaryapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.salary_form, name='salary_form'),
-    path('result/', views.salary_result, name='salary_result'),
-    path('jumble/', views.jumble_word, name='jumble_word'),
-]
